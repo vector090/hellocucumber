8747c54fa0d6cda7b719f39d77e03ed1cf306b54
James Fu <jamesfu@soundbus.cn>
2018-05-23 15:14:04 +0800

    Test Chinese user story and feature.

--- src/test/java/hellocucumber/UserStoryZhSteps.java
+++ src/test/java/hellocucumber/UserStoryZhSteps.java
@@ -20,4 +20,21 @@ public class UserStoryZhSteps {
     @那么("^我应该对Cucumber有基本的了解$")
     public void 我应该对cucumber有基本的了解() throws Exception {
     }
+
+
+    @假设("^我同事具有使用Cucumber编写用户故事和验收测试用例的经验$")
+    public void 我同事具有使用cucumber编写用户故事和验收测试用例的经验() throws Exception {
+    }
+
+    @当("^我学习同事编写的用户故事和验收测试用例$")
+    public void 我学习同事编写的用户故事和验收测试用例() throws Exception {
+    }
+
+    @当("^我学习Cucumber文档$")
+    public void 我学习cucumber文档() throws Exception {
+    }
+
+    @那么("^我应该学会用Cucumber编写用户故事和验收测试用例$")
+    public void 我应该学会用cucumber编写用户故事和验收测试用例() throws Exception {
+    }
 }
--- src/test/resources/hellocucumber/userStory-zh.feature
+++ src/test/resources/hellocucumber/userStory-zh.feature
@@ -1,7 +1,7 @@
 # language: zh-CN
 功能: 解释 Cucumber
   作为非程序员
-  我想对Cucumber有一个非技术性的总体了解
+  我想对Cucumber有一个非技术性的了解
   以便了解Cucumber测试系统
 
   场景: 老李想对Cucumber有一个总体了解
@@ -9,3 +9,9 @@
     当 我让同事向我总体性地介绍Cucumber如何工作
     并且 我听同事的解释
     那么 我应该对Cucumber有基本的了解
+
+  场景: 老李想了解如何用Cucumber管理用户故事和验收测试用例
+    假设 我同事具有使用Cucumber编写用户故事和验收测试用例的经验
+    当 我学习同事编写的用户故事和验收测试用例
+    并且 我学习Cucumber文档
+    那么 我应该学会用Cucumber编写用户故事和验收测试用例
