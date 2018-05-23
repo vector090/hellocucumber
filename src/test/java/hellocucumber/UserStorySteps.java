9517b1e3ea93b879f6b15ec57321dcc7c4d23c2e
James Fu <jamesfu@soundbus.cn>
2018-05-23 14:47:30 +0800

    Test user story with feature.

---
+++ src/test/java/hellocucumber/UserStorySteps.java
@@ -0,0 +1,23 @@
+package hellocucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class UserStorySteps {
+    @Given("^I have a coworker who knows a lot about Cucumber$")
+    public void i_have_a_coworker_who_knows_a_lot_about_Cucumber() throws Exception {
+    }
+
+    @When("^I ask my coworker to give an overview of how Cucumber works$")
+    public void i_ask_my_coworker_to_give_an_overview_of_how_Cucumber_works() throws Exception {
+    }
+
+    @When("^I listen to their explanation$")
+    public void i_listen_to_their_explanation() throws Exception {
+    }
+
+    @Then("^I should have a basic understanding of Cucumber$")
+    public void i_should_have_a_basic_understanding_of_Cucumber() throws Exception {
+    }
+}
---
+++ src/test/resources/hellocucumber/userStory.feature
@@ -0,0 +1,10 @@
+Feature: Explaining Cucumber
+  In order to gain an understanding of the Cucumber testing system
+  As a non-programmer
+  I want to have an overview of Cucumber that is understandable by non-geeks
+
+  Scenario: A worker seeks an overview of Cucumber
+    Given I have a coworker who knows a lot about Cucumber
+    When I ask my coworker to give an overview of how Cucumber works
+    And I listen to their explanation
+    Then I should have a basic understanding of Cucumber
