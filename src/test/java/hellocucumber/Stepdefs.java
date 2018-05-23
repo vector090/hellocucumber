4f0f3839fb8b5660f7ba8bc9b3c3758f6e3f18f6
James Fu <jamesfu@soundbus.cn>
2018-05-23 10:25:35 +0800

    new file:   .gitignore 	modified:   pom.xml 	modified:   src/test/java/hellocucumber/Stepdefs.java 	new file:   src/test/resources/hellocucumber/is_it_friday_yet.feature

--- src/test/java/hellocucumber/Stepdefs.java
+++ src/test/java/hellocucumber/Stepdefs.java
@@ -2,9 +2,25 @@ package hellocucumber;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
-import static org.junit.Assert.*;
+import cucumber.api.java.en.When;
 
 public class Stepdefs {
+    @Given("^today is Sunday$")
+    public void today_is_Sunday() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I ask whether it's Friday yet$")
+    public void i_ask_whether_it_s_Friday_yet() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
